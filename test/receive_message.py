b4179c4f8b87f5b0ec431883ce3bbee23c53dbd1
niushuaibing <niushuaibing@foxmail.com>
2019-12-26 15:27:24 +0800

    add example for using easymq

---
+++ test/receive_message.py
@@ -0,0 +1,17 @@
+import time
+
+from easymq.connect_mq import Connection, Listener
+
+
+class CustomListener(Listener):
+
+    def on_message(self, headers, message):
+        print("----->", message)
+
+
+if __name__ == '__main__':
+
+    a = Connection(mq_username="admin", mq_password="admin", host_and_ports=[("localhost", 61613)], use_ssl=False, listener=CustomListener)
+    while True:
+        a.receive(mq_destination=["/queue/collect_event", "/queue/collect.event"])
+        time.sleep(2)
